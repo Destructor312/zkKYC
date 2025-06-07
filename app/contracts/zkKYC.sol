42875ac21e1245072c615b6a913ff599f287156d
Destructor312 <vash2002person@gmail.com>
2025-06-07 16:41:12 +1000

    added remapping.txt and pulled openzeppelin library

--- app/contracts/zkKYC.sol
+++ app/contracts/zkKYC.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@semaphore-protocol/contracts/base/Semaphore.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 contract zkKYC is ERC721{
 
     function __verifyProof(){
